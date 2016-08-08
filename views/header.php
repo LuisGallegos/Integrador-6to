2886c4de3cb160f6185ea4669f9b94897af96504
Joss <mustaine900@gmail.com>
2016-08-08 18:03:16 -0500

    Final

--- views/header.php
+++ views/header.php
@@ -102,23 +102,7 @@
             <a ui-sref-active="active" ui-sref="candidats"><i class="fa fa-edit fa-fw"></i><span> Candidats</span></a>
         </li>
 
-        <li class="active treeview">
-          <a href="#">
-            <i class="fa fa-dashboard"></i> <span>LABELS</span>
-            <span class="pull-right-container">
-              <i class="fa fa-angle-left pull-right"></i>
-            </span>
-          </a>
-          <ul class="treeview-menu">
-            <li><a href="#"><i class="fa fa-circle-o text-red"></i> <span>Important</span></a></li>
-            <li><a href="#"><i class="fa fa-circle-o text-yellow"></i> <span>Warning</span></a></li>
-            <li><a href="#"><i class="fa fa-circle-o text-aqua"></i> <span>Information</span></a></li>
-          </ul>
-        </li>
-
-        <li>
-          <a href="documentation/index.html"><i class="fa fa-book"></i> <span>Documentation</span></a>
-        </li>
+        
 
       </ul>
     </section>
